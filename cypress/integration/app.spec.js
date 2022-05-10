0a16f32ec811031711029d48dc4fe40171d1f1dc
phoebus-84 <ennio@dyne.org>
2022-05-10 19:21:16 +0200

    test: add tests for process page

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,8 +1,27 @@
 describe('Navigation', () => {
-  it('should contain some info or invitation to login', () => {
+  it('should contain some info or login form', () => {
     cy.visit('/')
-    cy.contains(/Activity:transfer|Probably you should log in in some way/g)
-    cy.contains(/produce|Probably you should log in in some way/g)
-    cy.contains(/process|Probably you should log in in some way/g)
+    cy.contains(/Activity:transfer|Sign/g)
+    cy.contains(/produce|Sign/g)
+    cy.contains(/process|Sign/g)
   })
+
+  it('should render a basic layout', () => {
+    cy.visit('/')
+    cy.get('.navbar').contains('ReflowApp')
+    cy.get('.drawer').contains('processes')
+  })
+
+   it('should open a popup to produce some resource inside a process starting navigates from home', () => {
+     cy.viewport('macbook-13')
+    cy.visit('/')
+    cy.contains('processes').click()
+    cy.url().should('include', '/processes')
+     cy.get('.drawer-content>ul>li>a').first().click()
+     cy.url().should('include', '/processes/')
+     cy.get('.drawer-content>ul>li>label').contains('Produce').click()
+     cy.get('.modal-box>form>select').first().contains(/(?=.*rice)(?=.*Gown)(?=.*Soap)/).select('rice')
+     cy.get('.modal-box>form>select').eq(1).select(3)
+  })
+
 })
