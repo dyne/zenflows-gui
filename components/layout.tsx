270a32cf581ebf123107adcbac9a011ae7602a49
phoebus-84 <ennio@dyne.org>
2022-05-11 11:26:29 +0200

    fix: fix layout types

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import {NextPage} from "next";
+import React, {ReactNode} from 'react';
 import Sidebar from "./sidebar"
 import Topbar from "./topbar";
 
 type layoutProps = {
-    children: Element
+    children: ReactNode
 }
 
 
-const Layout:React.FunctionComponent<Element> = ({children}:layoutProps) => {
+const Layout:React.FunctionComponent<layoutProps> = (layoutProps:layoutProps) => {
     return (
         <>
             <Topbar/>
             <div className="drawer drawer-mobile" >
                 <input id = "my-drawer" type = "checkbox" className = "drawer-toggle" />
                 <div className="drawer-content">
-                    {children}
+                    {layoutProps?.children}
                 </div>
                 <div className="drawer-side">
                     <label htmlFor="my-drawer" className="drawer-overlay">barra laterale</label>
