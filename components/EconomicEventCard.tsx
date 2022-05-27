80e2c20fec23b5f5b5866b2af4bcd0490edbe6b7
phoebus-84 <ennio@dyne.org>
2022-05-27 19:12:30 +0200

    refactor: add daysiUI theme configuration

--- components/EconomicEventCard.tsx
+++ components/EconomicEventCard.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 import React from "react";
 
 const EconomicEventCards = ({event}:any) => (<>
-  <Card title={event.action?.id}>
+  <Card title={event?.action?.id}>
       <>
-        <li>note:{event.note}</li>
-        <li>from:{event.provider?.displayUsername}</li>
-        <li>to:{event.receiver?.displayUsername}</li>
-        <li>resource type::{event.resourceConformsTo?.name}</li>
-        <li><Link href={`/resource/${event.resourceInventoriedAs?.id || ''}`}><a>resource link</a></Link></li>
-        <li>quantity:{event.resourceQuantity?.hasNumericalValue} {event.resourceQuantity?.hasUnit.label}</li>
+        <li>note:{event?.note}</li>
+        <li>from:{event?.provider?.displayUsername}</li>
+        <li>to:{event?.receiver?.displayUsername}</li>
+        <li>resource type::{event?.resourceConformsTo?.name}</li>
+        <li><Link href={`/resource/${event?.resourceInventoriedAs?.id || ''}`}><a>resource link</a></Link></li>
+        <li>quantity:{event?.resourceQuantity?.hasNumericalValue} {event?.resourceQuantity?.hasUnit.label}</li>
       </>
   </Card>
 </>)
