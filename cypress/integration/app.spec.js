c171a9e1573e1774b9f34fa15a2f495372ec9746
phoebus-84 <ennio@dyne.org>
2022-05-30 11:30:21 +0200

    test: add some integration tests, needs cypress.env.json to have secrets

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,13 +1,19 @@
+
+
 describe('Navigation', () => {
-  it('should contain some info or login form', () => {
+  it('Should render a login Form. Should render index after login', () => {
     cy.visit('/')
-    cy.contains(/Activity:transfer|Sign/g)
-    cy.contains(/produce|Sign/g)
-    cy.contains(/process|Sign/g)
+    cy.get('form input:first').type(Cypress.env('user_name'))
+    cy.get('form input:nth-child(2)').type(Cypress.env('user_password'))
+    cy.get('form button').click()
+    cy.contains('Process')
   })
 
   it('should render a basic layout', () => {
     cy.visit('/')
+    cy.get('form input:first').type(Cypress.env('user_name'))
+    cy.get('form input:nth-child(2)').type(Cypress.env('user_password'))
+    cy.get('form button').click()
     cy.get('.navbar').contains('ReflowApp')
     cy.get('.drawer').contains('Processes')
   })
@@ -15,12 +21,28 @@ describe('Navigation', () => {
    it('should open a popup to produce some resource inside a process starting navigates from home', () => {
      cy.viewport('macbook-13')
      cy.visit('/')
+     cy.get('form input:first').type(Cypress.env('user_name'))
+     cy.get('form input:nth-child(2)').type(Cypress.env('user_password'))
+     cy.get('form button').click()
      cy.contains('Processes').click()
      cy.url().should('include', '/processes')
      cy.get('.drawer-content>ul>li>a').first().click()
      cy.url().should('include', '/processes/')
-     cy.get('.drawer-content>ul>li>label').contains('Produce').click()
+     cy.get('.drawer-content>ul>li>label').contains('produce').click()
      cy.get('.modal-box>form>select').first().contains(/(?=.*rice)(?=.*Gown)(?=.*Soap)/).select('rice')
      cy.get('.modal-box>form>select').eq(1).select(3)
+  })
+
+  it('should render resource page', () => {
+    cy.viewport('macbook-13')
+    cy.visit('/')
+    cy.get('form input:first').type(Cypress.env('user_name'))
+    cy.get('form input:nth-child(2)').type(Cypress.env('user_password'))
+    cy.get('form button').click()
+    cy.contains('Resources').click()
+    cy.url().should('include', '/my_inventory')
+    cy.get('.drawer-content>ul>li>a').first().click()
+    cy.url().should('include', '/resource/')
+    cy.get('.drawer-content').contains('economicResource')
   })
 })
