06b88dc023910400e18610b157120c406e79fac9
Puria Nafisi Azizi <puria@dyne.org>
2022-06-20 12:52:39 +0200

    feat: add next.js integration with zenroom

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  env:{
-    GRAPHQL:"https://reflow-demo.dyne.org/api/graphql",
-  }
+    reactStrictMode: true,
+    env: {
+        GRAPHQL: "https://reflow-demo.dyne.org/api/graphql",
+    },
+    webpack: (config) => {
+        config.resolve.fallback = {
+            fs: false,
+            process: false,
+            path: false,
+            crypto: false,
+        };
+
+        return config;
+    },
 }
 
 module.exports = nextConfig
