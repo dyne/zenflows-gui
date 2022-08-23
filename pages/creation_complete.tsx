74f69a9605bd6d98b841b68a4ecdfbde82cdbe79
phoebus-84 <ennio@dyne.org>
2022-08-23 08:31:51 +0200

    feat: add i18n internationalization

--- pages/creation_complete.tsx
+++ pages/creation_complete.tsx
@@ -1,12 +1,29 @@
 import Link from "next/link";
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useTranslation, Trans } from 'next-i18next'
+
+
 
 const CreationComplete = () => {
+    const { t } = useTranslation('common')
     return (
+        <>
         <div>
-            <h1>All set!</h1>
-            <Link href={'/'}><a className={'button button-primary'}>Go to home</a></Link>
-            <Link href={'/projects'}><a className={'button button-primary'}>Go to projects</a></Link>
-            <Link href={'/inventory'}><a className={'button button-primary'}>Go to inventory</a></Link>
+            <h1>{t('All set!')}</h1>
+            <Link href={'/'}><a className={'btn btn-primary'}>{t('Go to home')}</a></Link><br/>
+            <Link href={'/projects'}><a className={'btn btn-primary my-2'}>{t('Go to projects')}</a></Link><br/>
+            <Link href={'/inventory'}><a className={'btn btn-primary'}>{t('Go to inventory')}</a></Link><br/>
         </div>
+            </>
     );
 }
+
+export async function getStaticProps({ locale }:any) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])),
+    },
+  };
+}
+
+export default CreationComplete;
