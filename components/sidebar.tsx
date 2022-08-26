249ad1dc540114347672d36433841e72b1a49ff4
phoebus-84 <ennio@dyne.org>
2022-08-26 18:00:56 +0200

    fix: minor layout adjustments

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,50 +1,13 @@
 import React, {useState} from 'react';
 import Link from 'next/link';
 
-import {UserGroupIcon, ArrowNarrowRightIcon, ArrowNarrowLeftIcon} from "@heroicons/react/solid";
+import {UserGroupIcon} from "@heroicons/react/solid";
 import {ChatIcon, CubeIcon, HomeIcon, GlobeIcon, ChevronUpIcon, ChevronDownIcon} from "@heroicons/react/outline";
 import LoginBtn from "./LoginMenu";
-import SideBarMenu from "./brickroom/SideBarMenu";
 import {useRouter} from "next/router";
-import CreateProjectButton from "./NewProjectButton";
 import IfSideBarButton from "./brickroom/IfSideBarButton";
 
 const SideBarProps = {
-    newProcess: {text: "New Process", link: "/new_process"},
-    menu1: [{
-        name: "Home",
-        link: "/",
-        svg: <HomeIcon className="w-5 h-5 float-left mr-2"/>
-    },
-        {
-            name: "Local",
-            link: "/local",
-            svg: <UserGroupIcon className="w-5 h-5 float-left mr-2"/>,
-            disabled: true
-        },
-        {
-            name: "Federation",
-            link: "/fed",
-            svg: <GlobeIcon className="w-5 h-5 float-left mr-2"/>,
-            disabled: true
-        },],
-    menu: [
-        {
-            name: "Processes",
-            link: "/processes",
-            svg: "",
-            disabled: true
-        },
-
-        {
-            name: "Resources",
-            link: "/my_inventory",
-            disabled: true
-        },
-        {
-            name: "User Profile",
-            link: "/profile/my_profile",
-        }],
     home: {
         name: 'Home',
         link: "/",
@@ -56,7 +19,7 @@ const SideBarProps = {
     },
     MyInventory: {
         name: 'My inventory',
-        link: '/',
+        link: '/my_inventory',
         disabled: true,
     },
     lastUpdated: {
@@ -91,16 +54,16 @@ function Sidebar() {
     return (<>
             <div className="title overflow-y-auto w-72 text-primary-content bg-white border-r border-primary">
                 {!isNewProcess && <>
-                    <div className="w-auto">
+                    <div className="w-auto h-16 pt-4 mb-4 border-b border-primary">
                         <Link href="/">
                             <a>
-                                <div className="logo mx-auto my-4"/>
+                                <div className="logo mx-auto"/>
                             </a>
                         </Link>
                     </div>
                     <ul className="p-0">
                         <li>
-                            <IfSideBarButton text={'home'} link={'/'} svg={<HomeIcon className="w-5 h-5 float-left mr-2"/>} active={isActive('/')}/>
+                            <IfSideBarButton text={'home'} link={'/'} svg={<HomeIcon className="w-5 h-5 float-left mr-2"/>} active={isActive('/')} w={64}/>
                         </li>
                         <li tabIndex={0}>
                             <a className="ml-4 w-64 gap-2 pl-0 btn btn-ghost font-medium normal-case text-primary border-0 hover:bg-amber-200">
@@ -112,18 +75,18 @@ function Sidebar() {
                                     </>
                                 </button>
                             </a>
-                            {isAssetsMenuOpen && <ul className="p-2">
-                                <li><IfSideBarButton text={SideBarProps.createAsset.name} link={SideBarProps.createAsset.link} active={isActive(SideBarProps.createAsset.link)}/></li>
-                                <li><IfSideBarButton text={SideBarProps.MyInventory.name} link={SideBarProps.MyInventory.link} disabled={true}/></li>
-                                <li><IfSideBarButton text={SideBarProps.lastUpdated.name} link={SideBarProps.lastUpdated.link} disabled={true}/></li>
-                                <li><IfSideBarButton text={SideBarProps.seeAll.name} link={SideBarProps.seeAll.link} disabled={true}/></li>
+                            {isAssetsMenuOpen && <ul className="pl-4">
+                                <li><IfSideBarButton w="60" text={SideBarProps.createAsset.name} link={SideBarProps.createAsset.link} active={isActive(SideBarProps.createAsset.link)}/></li>
+                                <li><IfSideBarButton w="60" text={SideBarProps.MyInventory.name} link={SideBarProps.MyInventory.link} disabled/></li>
+                                <li><IfSideBarButton w="60" text={SideBarProps.lastUpdated.name} link={SideBarProps.lastUpdated.link} disabled/></li>
+                                <li><IfSideBarButton w="60" text={SideBarProps.seeAll.name} link={SideBarProps.seeAll.link} /></li>
                             </ul>}
                         </li>
                         <li>
-                            <IfSideBarButton text={SideBarProps.userGuide.name} link={SideBarProps.userGuide.link} svg={SideBarProps.userGuide.svg} active={isActive(SideBarProps.userGuide.link)} disabled={true}/>
+                            <IfSideBarButton text={SideBarProps.userGuide.name} link={SideBarProps.userGuide.link} svg={SideBarProps.userGuide.svg} active={isActive(SideBarProps.userGuide.link)} disabled={true} w={64}/>
                         </li>
                         <li>
-                            <IfSideBarButton text={SideBarProps.map.name} link={SideBarProps.map.link} svg={SideBarProps.map.svg} active={isActive(SideBarProps.map.link)} disabled={true}/>
+                            <IfSideBarButton text={SideBarProps.map.name} w={64} link={SideBarProps.map.link} svg={SideBarProps.map.svg} active={isActive(SideBarProps.map.link)} disabled={true}/>
                         </li>
                     </ul>
                     <span className="inline-block align-bottom">
