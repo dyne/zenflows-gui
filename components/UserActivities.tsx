59545527dcc3822d851d3c81584033ed7ed28e9c
phoebus-84 <ennio@dyne.org>
2022-09-06 10:08:52 +0200

    refactor: ttranslate homepage

--- components/UserActivities.tsx
+++ components/UserActivities.tsx
@@ -6,6 +6,10 @@ import Link from "next/link";
 import NewProcessButton from "./NewProcessButton";
 import Spinner from "./brickroom/Spinner";
 import CreateProjectButton from "./NewProjectButton";
+import {CheckCircleIcon} from "@heroicons/react/outline";
+import {useAuth} from "../lib/auth";
+import {serverSideTranslations} from "next-i18next/serverSideTranslations";
+import {useTranslation} from "next-i18next";
 
 
 const FETCH_USER_DATA = gql`
@@ -64,25 +68,21 @@ const FETCH_USER_DATA = gql`
             }
           }
         }`
-const HomeProps = {
-    welcome: {
-        title: "Welcome to Interfacer Alpha",
-        paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque pellentesque hendrerit ultrices mauris et non pellentesque suspendisse est.",
-    }
-}
 
 
-const User: NextPage = () => {
+const User = ({title, par1, par2}:{title:string, par1:string, par2:string}) => {
+    const { isSignedIn } = useAuth()
     const activities = useQuery(FETCH_USER_DATA).data?.me?.user.userActivities
     return <>
         <div className="flex justify-between mb-6">
-            <div className="w-96">
-                <h2>{HomeProps.welcome.title}</h2>
-                <p>{HomeProps.welcome.paragraph}</p>
+            <div className="w-128">
+                <div className="logo mb-4"/>
+                <h2>{title}</h2>
+                <p className="mt-4"><CheckCircleIcon className="w-5 h-5 float-left"/>{par1}</p>
+                <p className="mt-4"><CheckCircleIcon className="w-5 h-5 float-left"/>{par2}</p>
+                <Link href="/sign_in"><a className={`btn btn-primary mt-4 ${isSignedIn? 'btn-disabled' : ''}`}>sign in</a></Link>
             </div>
-            <div className="w-80">
-                <NewProcessButton/>
-                <CreateProjectButton/>
+            <div>
                 {/*<Link href="/processes">*/}
                 {/*    <a className="btn btn-outline font-medium normal-case btn-primary w-60 ml-4">*/}
                 {/*        See all process*/}
